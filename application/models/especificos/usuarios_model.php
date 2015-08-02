e193a0312a51c5ce930f6ffa0d372136491b8aad
Andres Valdes A <chicodx@gmail.com>
2015-08-02 18:10:16 -0400

    Con mantenedor de usuarios

---
+++ application/models/especificos/usuarios_model.php
@@ -0,0 +1,71 @@
+<?php
+
+class Usuarios_model extends CI_Model{
+    
+    function insertar($datos){
+        
+        if($this->db->insert('usuario', $datos)){
+            return true;
+        }
+        else{
+            return false;
+        }
+         
+    }
+    
+/*  
+    function modificar($datos,$codigo_tramo){
+        $this->db->where('codigo_tramo',$codigo_tramo);               
+        
+        if($this->db->update('tramo', $datos)){
+            return true;
+        }
+        else{
+            return false;
+        }
+         
+    }    
+ 
+ * 
+ */   
+        
+    function listar_usuarios(){
+        
+        $this->db->select('rut_usuario,nombre');
+        $resultado = $this->db->get('usuario');
+        
+        return $resultado->result_array();
+        
+    }
+    
+    function existe_usuario($usuario){
+        $this->db->select ('rut_usuario');
+        $this->db->from('usuario');
+        $this->db->where('rut_usuario',$usuario);
+                
+        $query = $this->db->get();
+        
+        if($query->num_rows() == 0){
+            
+            return true;
+        }
+        
+        else{
+            
+            return false;
+        }
+    }
+	
+	function datos_usuario($rut) {
+			$this->db->select ();
+			$this->db->from('usuario');
+			$this->db->where('rut_usuario',$rut);
+			$resultado = $this->db->get();
+			
+			return $resultado->result_array();
+        
+    }
+	
+}
+
+?>
