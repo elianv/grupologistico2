32a7f0ea076b329987be0f6e72c7fac7ea6bae30
Andres Valdes A <chicodx@gmail.com>
2015-08-03 22:50:32 -0400

    Ahora si

--- application/views/include/head.php
+++ application/views/include/head.php
@@ -73,10 +73,10 @@
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Espec&iacute;ficos<b class="caret"></b></a>
 							<ul class="dropdown-menu">
 								<li><a href="#">Fecha del d&iacute;a</a></li>
-								<li><a href=<?php echo base_url();?>index.php/especificos/usuarios>Usuarios</a></li>
+								<li><a href=<?php echo base_url('index.php/especificos/especificos/usuarios');?>">Usuarios</a></li>
 								<li><a href="#">Claves de Acceso</a></li>
 								<li><a href="#">Par&aacute;metros</a></li>
-								<li><a href="<?php echo base_url('index.php/especificos/especificos/codigos_sistema');?>">C&oacute;digos Sistema</a></li>
+								<li><a href=<?php echo base_url('index.php/especificos/especificos/codigos_sistema');?>">C&oacute;digos Sistema</a></li>
 							</ul>
 						</li>
 					</ul>
