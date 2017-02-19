4e77d9a5fdd54e54d9dafea617eb0dda00a847e4
elian vallejos <elian.vallejos@gmail.com>
2017-02-18 23:27:11 -0300

    se elimina la busqueda de fecha presentacion en master.. solo se deja fecha de factura

--- application/models/consultas/consultas_model.php
+++ application/models/consultas/consultas_model.php
@@ -535,8 +535,8 @@ Class consultas_model extends CI_Model{
 					$desde = new DateTime($desde);
 					$hasta = new DateTime($hasta);
 
-					$string .= " AND orden.fecha_presentacion between '".$desde->format('Y-m-d')."' and '".$hasta->format('Y-m-d')."'";
-					$string .= " OR orden.id_orden IN (SELECT id_orden FROM ordenes_facturas WHERE id_factura IN ( SELECT id FROM factura WHERE fecha > '{$desde->format('Y-m-d')}' AND fecha < '{$hasta->format('Y-m-d')}' )) ";
+					//$string .= " AND orden.fecha_presentacion between '".$desde->format('Y-m-d')."' and '".$hasta->format('Y-m-d')."'";
+					$string .= " AND orden.id_orden IN (SELECT id_orden FROM ordenes_facturas WHERE id_factura IN ( SELECT id FROM factura WHERE fecha > '{$desde->format('Y-m-d')}' AND fecha < '{$hasta->format('Y-m-d')}' )) ";
 					$query .= $string;
 			}
 
