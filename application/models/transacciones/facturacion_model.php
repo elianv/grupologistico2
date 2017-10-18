331d6e9167a02b2776493ad1075f557dbcd3e299
elianv <elian.vallejos@gmail.com>
2017-10-18 00:00:26 -0300

    Problema de sincronizacion

--- application/models/transacciones/facturacion_model.php
+++ application/models/transacciones/facturacion_model.php
@@ -390,9 +390,9 @@ class Facturacion_model extends CI_Model{
     }
 
     function sincronizarFact($id,$num_factura,$fecha_manager=null){
-            echo "UPDATE factura SET estado_factura_id_estado_factura = 2, numero_factura = {$num_factura}, fecha='{$fecha_manager}' WHERE id = {$id}";
-          $sql = $this->db->query("UPDATE factura SET estado_factura_id_estado_factura = 2, numero_factura = {$num_factura}, fecha='{$fecha_manager}' WHERE id = {$id}");
-          return TRUE;
+            //echo "UPDATE factura SET estado_factura_id_estado_factura = 2, numero_factura = {$num_factura}, fecha='{$fecha_manager}' WHERE id = {$id}";
+            $sql = $this->db->query("UPDATE factura SET estado_factura_id_estado_factura = 2, numero_factura = {$num_factura}, fecha='{$fecha_manager}' WHERE id = {$id}");
+            return TRUE;
     }
 
     function actualizarOS($id){
