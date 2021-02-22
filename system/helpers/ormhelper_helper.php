8716ce4fc600bd56bb16cb12bc4e400d561657a9
elianv <elian.vallejos@gmail.com>
2021-02-22 02:19:23 -0300

    agrega curls para hacer ocr del pdf y se guarda datos

--- system/helpers/ormhelper_helper.php
+++ system/helpers/ormhelper_helper.php
@@ -1,88 +1,93 @@
 <?php
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 if ( ! function_exists('get_text')){
     function get_text($files) {
-        
+
         $CI = & get_instance();
         $session_data   = $CI->session->userdata('logged_in');
         $ruta = '/'.$session_data['rut_usuario'] .'_'.date('s_u');
         if (!is_dir(sys_get_temp_dir().$ruta)){
             mkdir(sys_get_temp_dir().$ruta, 01777);
         }
-        
 
         $i = 0;
-        
+
         foreach ($files['orden_file']['tmp_name'] as $f) {
-            
+
             try{
-                $file_name = str_replace(' ', '_', $files['orden_file']['name'][$i]);
-                //print(file_get_contents($f));
+                $ch = curl_init();
 
-                /* PDF TEMP A JPG */
-                $destino = sys_get_temp_dir() . $ruta. '/' . $file_name . '.jpg';
-                $command = 'convert -append -density 800 ' . $f . '[0-2] ' . $destino;
-                shell_exec($command);
+                shell_exec('cp ' . $f . ' ' . $f . '.pdf');
+                if (function_exists('curl_file_create')) { // php 5.5+
+                  $cFile = curl_file_create($f.'.pdf');
+                }
+                else { //
+                  $cFile = '@' . realpath($f.'.pdf');
+                }
 
-                /* JPG A TEXTO */
-                $origen = sys_get_temp_dir() . $ruta . '/' . $file_name . '.jpg';
-                $destino = sys_get_temp_dir() . $ruta . '/' . $file_name;
+                $postData = array(
 
-                $command = 'tesseract ' . $origen . ' ' .$destino;
-                shell_exec($command);
+                    'File' => $cFile,
+                    'StoreFile' => 'true',
+                );
 
-                // LEO EL TXT
-                /*
-                echo '<pre>';
-                echo file_get_contents($destino.'.txt');
-				echo '</pre>';
-                */
+                //curl_setopt($ch, CURLOPT_SAFE_UPLOAD, false);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_URL,"https://v2.convertapi.com/convert/pdf/to/txt?Secret=82WC01CaY1kkxI71");
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-                $f_text = fopen($destino.'.txt', 'r');
-                $texto = fread($f_text, filesize($destino.'.txt'));
-                fclose($f_text);
-                  
-                // ELIMINO TODOS LOS ARCHIVOS EN RUTA TEMPORAL
-                $command = 'rm ' . sys_get_temp_dir().$ruta . '/*.*';
-                shell_exec($command);
-                
-                //SALIDA FUE OK, PASO AL SIGUIENTE
-                $salida = true;
-            
+                $server_output = curl_exec($ch);
+                curl_close ($ch);
+
+                $data = json_decode($server_output, true);
+
+                $curl_handle=curl_init();
+                curl_setopt($curl_handle, CURLOPT_URL, $data['Files'][0]['Url']);
+                curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+                curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($curl_handle, CURLOPT_USERAGENT, 'SCT');
+                $query = curl_exec($curl_handle);
+                curl_close($curl_handle);
+
+                $file_name = $f;
+                $texto = $query;
+                $salida = 1;
+
             }
             catch ( Exception $e){
-                
+
                 $texto = $e;
                 $salida = false;
-                
+
             }
-            
+
             $a[$i]['nombre'] = $file_name;
             $a[$i]['status'] = $salida;
             $a[$i]['texto'] = $texto;
             $i++;
-            
+
         }
-        
+
         try{
             //rmdir(sys_get_temp_dir().$ruta);
         } catch (Exception $ex) {
-            
+
         }
 
         return $a;
     }
 }
 
-
 if ( !function_exists('test_txt')){
     function test_txt(){
-        
+
         for ($i=0; $i<1; $i++){
             $f_text = fopen('/tmp/out.jpg.txt', 'r');
             $texto = fread($f_text, filesize('/tmp/out.jpg.txt'));
@@ -91,17 +96,17 @@ if ( !function_exists('test_txt')){
                 'nombre'=>'nombre',
                 'status'=> true,
                 'texto'=>$texto,
-                );            
+                );
         }
-        
+
         return $a;
     }
 }
 /* COMANDO PARA CONVERTIR
- * 
+ *
  * convert -append -density 1000 ZLE_FM45A_20191210165924.pdf out.jpg
  * tesseract out-0.jpg out.txt
- * 
- * 
- * 
+ *
+ *
+ *
  */
