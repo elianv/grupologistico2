c730edd9101b3f3c6463c7b4cbc8f58fe8857fbb
elianv <elian.vallejos@gmail.com>
2021-03-21 00:41:20 -0300

    Correccion informe

--- application/views/consultas/facturadas.php
+++ application/views/consultas/facturadas.php
@@ -120,7 +120,7 @@
                                         <tr>
                                             <td><a href="<?php echo base_url('index.php/transacciones/orden/pdf/'.$cliente['id_orden'])?>" title="Para ver la Orden haga click"><?php echo $cliente['id_orden']; ?></a></td>
                                             <td><?php echo $cliente['tipo_orden']; ?></td>
-                                            <?php $fecha = new DateTime($cliente['fecha_creacion']); ?>
+                                            <?php $fecha = new DateTime($cliente['fecha_presentacion']); ?>
                                             <td><?php echo $fecha->format('d-m-Y'); ?></td>
                                             <td><?php echo'$'.number_format($cliente['total_neto'], 0, ',', '.'); ?></td>
                                             <td><a href="<?php echo base_url('index.php/transacciones/facturacion/imprimir/'.$cliente['numero_factura']); ?>" target="_blank"><?php echo $cliente['numero_factura']; ?></a></td>
